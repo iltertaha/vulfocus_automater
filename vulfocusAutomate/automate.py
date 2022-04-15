1896b421533c37f1a09e42ea6ae1b59ed5e296fb
iltertaha <ilteraktolga@gmail.com>
2022-04-15 20:51:24 +0300

    Update automate.py

--- vulfocusAutomate/automate.py
+++ vulfocusAutomate/automate.py
@@ -1,4 +1,7 @@
-import os
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+print( "Current directory: "+ os.path.dirname(os.path.realpath(__file__)))
+
 
 VERBOSE = False
 
